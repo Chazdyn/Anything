ca91519518580907c986eb37e87c17454d45978c
Chazdyn <chazdyns@gmail.com>
2026-05-01 11:41:25 -0700

    Add files via upload

---
+++ control-dyn/sw.js
@@ -0,0 +1,37 @@
+// Control.Dyn Service Worker
+const CACHE = 'controldyn-v1';
+const ASSETS = [
+  './',
+  './Control.Dyn.html',
+  './control.css',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  // Network first for GitHub API calls, cache first for app shell
+  if (e.request.url.includes('api.github.com') || e.request.url.includes('api.twitch.tv') || e.request.url.includes('id.twitch.tv')) {
+    e.respondWith(fetch(e.request).catch(() => new Response('{}', {headers:{'Content-Type':'application/json'}})));
+    return;
+  }
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).then(res => {
+      const clone = res.clone();
+      caches.open(CACHE).then(c => c.put(e.request, clone));
+      return res;
+    }))
+  );
+});
